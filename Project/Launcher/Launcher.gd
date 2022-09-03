b1a43f22e7a20c5b9a3d6ab74c5195715e41b70d
wendallR <wendallar125@gmail.com>
2022-09-03 13:45:33 -0400

    Give Launcher Rotation

--- Project/Launcher/Launcher.gd
+++ Project/Launcher/Launcher.gd
@@ -1,15 +1,38 @@
 extends Node2D
 
 var power := 200
-var angle := 45
-
+var angle := 0.0
+var rotationSpeed := 0.1
+var clockwise := false
 var _launched := false
-func _ready():
-	$LauncherSprite/Projectile.hide()
+
+func adjustAngle():
+	
+	if clockwise:
+		if angle <= 0:
+			clockwise = false
+		else:
+			angle -= rotationSpeed
+	else:
+		if angle >= 90:
+			clockwise = true
+		else:
+			angle += rotationSpeed
+	return angle
+	
+
+
 
 func _process(_delta):
-	if not _launched and  Input.is_action_just_pressed("launch"):
-		var impulse := Vector2(power,0).rotated(deg2rad(-angle))
-		$LauncherSprite/Projectile.show()
-		$LauncherSprite/Projectile.apply_impulse(Vector2.ZERO, impulse )
-		_launched = true
+	
+	if not _launched:
+		
+		if Input.is_action_just_pressed("launch"):
+			var impulse := Vector2(power,0).rotated(deg2rad(-angle))
+			$LauncherSprite/Projectile.gravity_scale = 1
+			$LauncherSprite/Projectile.apply_impulse(Vector2.ZERO, impulse )
+			_launched = true
+		adjustAngle()
+		$LauncherSprite.rotation = deg2rad(-angle)
+	
+	
