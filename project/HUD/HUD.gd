cb5dc1a04277a0cd9983b8c1dd60b6993538d96d
wendallR <wendallar125@gmail.com>
2022-09-08 19:20:14 -0400

    Add Projectile Count to UI

--- project/HUD/HUD.gd
+++ project/HUD/HUD.gd
@@ -14,7 +14,10 @@ func _set_launcher(value):
 	$PowerMeter.min_value = launcher.minPower
 	$PowerMeter.max_value = launcher.maxPower
 	launcher.connect("power_changed", self, "_on_Launcher_power_changed")
-
+	
+	_on_projectile_count_changed(launcher.projectilesLeft)
+	launcher.connect("projectile_count_changed", self, "_on_projectile_count_changed")
+	
 
 func _on_Launcher_angle_changed(new_angle):
 	_update_angle_label(new_angle)
@@ -28,3 +31,10 @@ func _on_Launcher_power_changed(new_power):
 	
 func _update_power_scroll_bar(power):
 	$PowerMeter.value = power
+
+func _on_projectile_count_changed(new_count):
+	_update_projectile_count(new_count)
+
+func _update_projectile_count(projectileCount):
+	$VBoxContainer/ProjectilesLeft.text = "Projectiles left: %d" % projectileCount
+	
--- project/Launcher/Launcher.gd
+++ project/Launcher/Launcher.gd
@@ -9,12 +9,12 @@ var _activeProjectile
 var _powerChangeRate := 1
 export var maxPower := 500
 export var minPower := 100
-export var _projectilesLeft := 2
+export var projectilesLeft := 3
 
 signal angle_changed(new_angle)
 signal power_changed(new_power)
-signal projectile_launched(launched)
-
+signal projectile_count_changed(new_count)
+signal projectile_launched(new_launch)
 
 
 func adjustAngle():
@@ -24,17 +24,17 @@ func adjustAngle():
 		rotate_counterclockwise()
 	emit_signal("angle_changed", angle)
 
+
 func rotate_counterclockwise():
 	angle += rotationSpeed
 	if angle >= 90.0:
 		clockwise = true
 
+
 func rotate_clockwise():
 	angle -= rotationSpeed
 	if angle <= 0.0:
 		clockwise = false
-	
-
 
 
 func _process(_delta):
@@ -52,18 +52,17 @@ func _process(_delta):
 			
 			_activeProjectile = preload("res://Projectile/Projectile.tscn").instance()
 			add_child(_activeProjectile)
-			_projectilesLeft -= 1
+			projectilesLeft -= 1
+			emit_signal("projectile_count_changed", projectilesLeft)
 			_activeProjectile.launch(impulse)
 			_activeProjectile.connect("sleeping_state_changed", self, "_on_Projectile_sleeping_state_changed")
-			
 			_launched = true
-			emit_signal("power_changed", minPower)
+			
 		adjustAngle()
 		rotation = deg2rad(-angle)
-	
-	
-func _on_Projectile_sleeping_state_changed():
 
+
+func _on_Projectile_sleeping_state_changed():
 	_activeProjectile.queue_free()
-	if _projectilesLeft > 0:
+	if projectilesLeft > 0:
 		_launched = false
