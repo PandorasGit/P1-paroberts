da377476c420403ce5d55272d17e2ee23ebd848e
wendallR <wendallar125@gmail.com>
2022-09-13 15:58:34 -0400

    Give Target Object DetonationTimer

--- project/Target/Target.gd
+++ project/Target/Target.gd
@@ -1,22 +1,19 @@
 extends RigidBody2D
 
 var _active := false
-var _timeToExplode := 2400
 signal activated()
 
 
 func _process(_delta):
-	if _timeToExplode <= 0:
-		explode()
+
 	
 	if linear_velocity != Vector2(0,0) and not _active:
 		_active = true
 		emit_signal("activated")
 		$Explosion.emitting = true
-	
-	if _active:
-		_timeToExplode -= 10
+		_removeTarget()
 
-func explode():
-	
+func _removeTarget():
+	$DetonationTimer.start()
+	yield($DetonationTimer, "timeout")
 	queue_free()
