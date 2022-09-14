b6ebaf70c449a6da86e615f39e74be9b641f9111
wendallR <wendallar125@gmail.com>
2022-09-14 19:02:26 -0400

    Create Main Menu

---
+++ project/MainMenu/MainMenu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var _world = preload("res://World/World.tscn")
+
+func _ready():
+	var _ignore = $StartButton.connect("pressed", self, "_on_StartButton_Pressed")
+	
+
+func _on_StartButton_Pressed():
+	var _ignore = get_tree().change_scene_to(_world)
+
+
